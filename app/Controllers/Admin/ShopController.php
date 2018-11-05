c8e19f360a5d6bd77f2a68329b6dd03b3d2aad0b
ccwy <ccwy@users.noreply.github.com>
2018-11-05 16:53:19 +0800

    99

--- app/Controllers/Admin/ShopController.php
+++ app/Controllers/Admin/ShopController.php
@@ -198,8 +198,8 @@ class ShopController extends AdminController
 
     public function bought($request, $response, $args)
     {
-        $table_config['total_column'] = array("op" => "操作", "id" => "ID", "datetime" => "购买日期","content" => "内容","price" => "价格", "user_id" => "用户ID","user_name" => "用户名", //"renew" => "自动续费时间", //"auto_reset_bandwidth" => "续费时是否重置流量"
-		"auto_reset_day" => "每月是否重置流量" );
+        $table_config['total_column'] = array("op" => "操作", "id" => "ID", "datetime" => "购买日期","price" => "价格", "user_id" => "用户ID","user_name" => "用户名", //"renew" => "自动续费时间", //"auto_reset_bandwidth" => "续费时是否重置流量"
+		"auto_reset_day" => "每月是否重置流量", "content" => "内容");
         $table_config['default_show_column'] = array();
         foreach ($table_config['total_column'] as $column => $value) {
             array_push($table_config['default_show_column'], $column);
